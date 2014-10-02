1c2129c381ddf057ba85acaae47c81d5f6673d2b
SpeedShifter <speeedermak@gmail.com>
2014-10-02 16:29:16 +0300

    Typescript integration

---
+++ tasks/inc.d.ts
@@ -0,0 +1,4 @@
+/// <reference path="../bower_components/DefinitelyTyped/node/node.d.ts" />
+/// <reference path="../bower_components/DefinitelyTyped/nodeunit/nodeunit.d.ts" />
+/// <reference path="../bower_components/DefinitelyTyped/lodash/lodash.d.ts" />
+/// <reference path="../bower_components/DefinitelyTyped/i18next/i18next.d.ts" />
--- tasks/static_i18next.js
+++ tasks/static_i18next.js
@@ -1,51 +1,28 @@
-/*
- * grunt-static-i18next
- * 
- *
- * Copyright (c) 2014 Stas Yermakov
- * Licensed under the MIT license.
- */
-
 'use strict';
-
 module.exports = function (grunt) {
+    grunt.registerMultiTask('static_i18next', 'Grunt plugin for localizing static assets', function () {
+        var options = this.options({
+            punctuation: '.',
+            separator: ', '
+        });
 
-  // Please see the Grunt documentation for more information regarding task
-  // creation: http://gruntjs.com/creating-tasks
+        this.files.forEach(function (file) {
+            var src = file.src.filter(function (filepath) {
+                if (!grunt.file.exists(filepath)) {
+                    grunt.log.warn('Source file "' + filepath + '" not found.');
+                    return false;
+                } else {
+                    return true;
+                }
+            }).map(function (filepath) {
+                return grunt.file.read(filepath);
+            }).join(grunt.util.normalizelf(options.separator));
 
-  grunt.registerMultiTask('static_i18next', 'Grunt plugin for localizing static assets', function () {
+            src += options.punctuation;
 
-    // Merge task-specific and/or target-specific options with these defaults.
-    var options = this.options({
-      punctuation: '.',
-      separator: ', '
+            grunt.file.write(file.dest, src);
+
+            grunt.log.writeln('File "' + file.dest + '" created.');
+        });
     });
-
-    // Iterate over all specified file groups.
-    this.files.forEach(function (file) {
-      // Concat specified files.
-      var src = file.src.filter(function (filepath) {
-        // Warn on and remove invalid source files (if nonull was set).
-        if (!grunt.file.exists(filepath)) {
-          grunt.log.warn('Source file "' + filepath + '" not found.');
-          return false;
-        } else {
-          return true;
-        }
-      }).map(function (filepath) {
-        // Read file source.
-        return grunt.file.read(filepath);
-      }).join(grunt.util.normalizelf(options.separator));
-
-      // Handle options.
-      src += options.punctuation;
-
-      // Write the destination file.
-      grunt.file.write(file.dest, src);
-
-      // Print a success message.
-      grunt.log.writeln('File "' + file.dest + '" created.');
-    });
-  });
-
 };
